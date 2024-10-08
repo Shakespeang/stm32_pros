58e4b9811a3ea3d5296728d25eb390c2434b689c
Shakespeang <992453893@qq.com>
2024-10-08 22:15:39 +0800

    把while1引了

--- freertos/TEMP/Core/Src/main.c
+++ freertos/TEMP/Core/Src/main.c
@@ -107,12 +107,12 @@ int main(void)
   /* We should never get here as control is now taken by the scheduler */
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+  //while (1)
+  //{
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  }
+  //}
   /* USER CODE END 3 */
 }
 
